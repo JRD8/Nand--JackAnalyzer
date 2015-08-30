171d0c3079d5e62e91a3ec08ddd005a5aae2f353
JRD8 <jdforbew@gmail.com>
2015-08-29 23:13:45 -0400

    Code Stubs for JackTokenizer routines

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -1,1 +1,50 @@
-print "hello world"
+################################
+####### JRD JACK ANALYZER ######
+################################
+
+## JACK TOKENIZER MODULE ##
+
+
+def constructor(input_file_or_stream):
+    return
+
+
+def hasMoreTokens():
+    result = false
+    return result
+
+
+def advance():
+    return
+
+
+def tokenType():
+    current_token_type = ""
+    return current_token_type
+
+
+def keyWord():
+    current_token_keyword = ""
+    return current_token_keyword
+
+
+def symbol():
+    current_token_char = ""
+    return current_token_char
+
+
+def identifier():
+    current_token_identifier = ""
+    return current_token_identifier
+
+
+def intVal():
+    current_intVal = 0 # Null Int?
+    return current_intVal
+
+
+def stringVal():
+    current_stringVal = ""
+    return current_stringVal
+
+
