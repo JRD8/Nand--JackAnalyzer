3a94978a3311ec1666aa1737d0fb1f7b454b54db
JRD8 <jdforbew@gmail.com>
2015-08-30 12:21:36 -0400

    Working on Constructor routine and process file input/defined a main route

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -5,6 +5,38 @@
 ## JACK TOKENIZER MODULE ##
 
 
+def main():
+    
+    # Import Jack file form command line script
+    from sys import argv
+    print "JRD Nand-2-Tetris Jack Analyzer, 2015\n"
+    print "Enter the Source Jack File (.jack) or Source Jack Directory (within this path) to be analyzed:"
+    source_input = raw_input(">") # User inputs source...
+    
+    ## These are the test Jack files.  Uncomment to substitute for source_input ##
+    #...
+    
+    print "\nThis is the Source: " + source_input
+    
+    if source_input.find(".jack") == -1: # Directory input
+        input_type = "directory"
+        print "(Source is a Directory Input)\n"
+        import os
+        script_path = os.path.dirname(ps.path.abspath(__file__))
+        source_path = script_path + "/" + source_input + "/*.jack"
+        print "Source Path = " + source_path + "\n"
+    
+    if source_input.find(".jack") != -1:
+        input_type = "file"
+        print "(Source is a File Input)\n"
+        source_path = ""
+    
+    source_name = source_input
+    print "This is the Source Name: " + source_name
+
+    return
+
+
 def constructor(input_file_or_stream):
     return
 
@@ -48,3 +80,5 @@ def stringVal():
     return current_stringVal
 
 
+# Process main routine
+main()
